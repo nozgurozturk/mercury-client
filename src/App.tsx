d96c446528ea739f296b8403602e9e066deaa992
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-09-10 23:12:27 +0300

    Add title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
-import {SPTheme} from "./utils/styles/SPTheme";
+import { ThemeProvider } from 'styled-components';
+import { SPTheme } from "./utils/styles/SPTheme";
 
- 
+import { Title } from './components/Title';
+
+
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={SPTheme}>
-        <></>
+      <div>
+        <Title title="projects" />
+        <Title title="guide and rules" />
+      </div>
     </ThemeProvider>
   );
 }
