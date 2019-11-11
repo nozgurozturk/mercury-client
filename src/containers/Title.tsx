779fb8f130227836472f95da05b9c8f67059e476
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-11-11 22:09:55 +0300

    Add : title and feth user data

--- src/containers/Title.tsx
+++ src/containers/Title.tsx
@@ -1,9 +1,44 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import styled from 'styled-components'
 
-type Props = {
-title: string
+const Header = styled.h1`
+    color:${props => props.theme.colors.main};
+    font-size:${props => props.theme.fontSize.header};
+    font-family:RoobertBold;
+`
+
+export const Title: FunctionComponent = () => {
+
+    const [name, setName] = useState<string>('');
+
+    const getUser = async () => {
+        const id: any = parseInt(localStorage.getItem('user')!)
+        const token: any = 'Bearer ' + localStorage.getItem('token')!.toString().replace(/"/g, "")
+        try {
+            let myHeaders = new Headers({
+                "Authorization": token,
+                "Content-Type": "application/json"
+            });
+            const response = await fetch(
+                `http://localhost:8080/user/${id}`,
+                {
+                    headers: myHeaders,
+                    method: "GET",
+                }
+            )
+            const json = await response.json()
+            setName(json.name)
+            console.log(json);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getUser();
+    }, [])
+
+    return (
+        <Header>Hello {name}</Header>
+    )
 }
-
-export const ComponentName: FunctionComponent<Props> = ({ title }) => (
-<h1>{title}</h1>
-)
