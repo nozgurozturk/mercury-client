80d21c61af7908cfcc1289b502cb811357967bb3
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-11-12 22:11:48 +0300

    Add : shortcuts container

--- src/containers/Shortcuts.tsx
+++ src/containers/Shortcuts.tsx
@@ -1,9 +1,38 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+import {Item} from '../components/Item';
 
 type Props = {
-title: string
+    title: string
 }
 
-export const ComponentName: FunctionComponent<Props> = ({ title }) => (
-<h1>{title}</h1>
-)
+const ShortcutTitle = styled.h3`
+    color:${props => props.theme.colors.main};
+    font-size:${props => props.theme.fontSize.header};
+    font-family:RoobertBold;
+    margin-bottom:16px;
+`;
+
+const ShortcutContainer = styled.ul`
+    display:grid;
+    grid-auto-flow:column;
+    justify-content:flex-start;
+    column-gap:48px;
+    overflow-x:scroll;
+`;
+
+export const Shortcuts: FunctionComponent = () => {
+
+    return (
+        <>
+        <ShortcutTitle>Shortcuts</ShortcutTitle>
+        <ShortcutContainer>
+            <Item type='shortcut' itemName='github'/>
+            <Item type='shortcut' itemName='jira'/>
+            <Item type='shortcut' itemName='figma'/>
+            <Item type='shortcut' itemName='react'/>
+        </ShortcutContainer>
+        </>
+    )
+}
