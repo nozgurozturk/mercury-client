e9bc48c1483000d94b72af0948bb09c00a6f7526
n.ozgurozturk <ozgur@nozgurozturk.com>
2019-11-12 22:12:13 +0300

    Add : time component

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,9 +1,42 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-type Props = {
-title: string
+const TimeContainer = styled.div`
+    display:grid;
+    grid-auto-flow:row;
+`;
+
+const TimeStyle = styled.div`
+    color:${props => props.theme.colors.main};
+    font-family:RoobertBold;
+    text-align:right;
+`;
+
+const Day = styled(TimeStyle)`
+    font-size:${props => props.theme.fontSize.body};
+`;
+
+const Hour = styled(TimeStyle)`
+    font-size:${props => props.theme.fontSize.header};
+`;
+
+export const Time: FunctionComponent = () => {
+    const [day, setDay] = useState<string>('')
+    const [hour, setHour] = useState<string>('')
+
+    useEffect(() => {
+        const currentTime = setInterval(() => {
+            const date = new Date()
+            setDay(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`)
+            setHour(`${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`)
+        }, 1000)
+        return () => clearInterval(currentTime)
+    }, [])
+
+    return (
+        <TimeContainer>
+            <Day>{day}</Day>
+            <Hour>{hour}</Hour>
+        </TimeContainer>
+    )
 }
-
-export const ComponentName: FunctionComponent<Props> = ({ title }) => (
-<h1>{title}</h1>
-)
